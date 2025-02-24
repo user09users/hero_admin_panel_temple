e8318b7ff0bd6e8979f32721bf342b13c7a74e7d
Mkh <example@gmail.com>
2025-02-24 12:37:18 +0100

    Redux_middleware_thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,21 @@
-/* import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default store; */
-
-import { createStore, combineReducers, compose } from 'redux';
-import heroes from '../reducers/heroes';
-import filters from '../reducers/filters';
-
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store;
-}
+    return next(action)
+};
 
 const store = createStore( 
-                    combineReducers({heroes, filters}),
-                    compose(
-                        enhancer,
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    ));
+                combineReducers({heroes, filters}),
+                compose(applyMiddleware(thunk, stringMiddleware),
+                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                );
 
 export default store;
