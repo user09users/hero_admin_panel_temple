b14652dd742bb82aad1345aa9fd9e29d33a58994
Mkh <example@gmail.com>
2025-02-27 11:06:41 +0100

    Redux_RTX_Query

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,69 +1,75 @@
-import { useHttp } from "../../hooks/http.hook";
-import { useEffect, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
-import {
-	heroDeleted,
-	fetchHeroes,
-	filteredHeroesSelector,
-} from "./heroesSlice";
+import { useGetHeroesQuery, useDeleteHeroMutation } from '../../api/apiSlice';
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from "../spinner/Spinner";
+import Spinner from '../spinner/Spinner';
 
-import "./heroesList.scss";
+import './heroesList.scss';
 
 const HeroesList = () => {
-	const filteredHeroes = useSelector(filteredHeroesSelector);
-	const heroesLoadingStatus = useSelector(
-		(state) => state.heroes.heroesLoadingStatus
-	);
-	const dispatch = useDispatch();
-	const { request } = useHttp();
+    const {
+        data: heroes = [],
+        isLoading,
+        isError,
+    } = useGetHeroesQuery();
 
-	useEffect(() => {
-		dispatch(fetchHeroes());
-		// eslint-disable-next-line
-	}, []);
+    const [deleteHero] = useDeleteHeroMutation();
 
-	const onDelete = useCallback(
-		(id) => {
-			request(`http://localhost:3001/heroes/${id}`, "DELETE")
-				.then((data) => console.log(data, "Deleted"))
-				.then(dispatch(heroDeleted(id)))
-				.catch((err) => console.log(err));
-			// eslint-disable-next-line
-		},
-		[request]
-	);
+    const activeFilter = useSelector(state => state.filters.activeFilter);
 
-	if (heroesLoadingStatus === "loading") {
-		return <Spinner />;
-	} else if (heroesLoadingStatus === "error") {
-		return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
-	}
+    const filteredHeroes = useMemo(() => {
+        const filteredHeroes = heroes.slice();
 
-	const renderHeroesList = (arr) => {
-		if (arr.length === 0) {
-			return (
-				<CSSTransition timeout={0} classNames="hero">
-					<h5 className="text-center mt-5">Героев пока нет</h5>
-				</CSSTransition>
-			);
-		}
+        if (activeFilter === 'all') {
+            return filteredHeroes;
+        } else {
+            return filteredHeroes.filter(item => item.element === activeFilter);
+        }
+    }, [heroes, activeFilter]);
 
-		return arr.map(({ id, ...props }) => {
-			return (
-				<CSSTransition key={id} timeout={500} classNames="hero">
-					<HeroesListItem {...props} onDelete={() => onDelete(id)} />
-				</CSSTransition>
-			);
-		});
-	};
+    const onDelete = useCallback((id) => {
+        deleteHero(id);
+        // eslint-disable-next-line  
+    }, []);
 
-	const elements = renderHeroesList(filteredHeroes);
-	return <TransitionGroup component="ul">{elements}</TransitionGroup>;
-};
+    if (isLoading) {
+        return <Spinner/>;
+    } else if (isError) {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return (
+                <CSSTransition
+                    timeout={0}
+                    classNames="hero">
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            )
+        }
+
+        return arr.map(({id, ...props}) => {
+            return (
+                <CSSTransition 
+                    key={id}
+                    timeout={500}
+                    classNames="hero">
+                    <HeroesListItem  {...props} onDelete={() => onDelete(id)}/>
+                </CSSTransition>
+            )
+        })
+    }
+
+    const elements = renderHeroesList(filteredHeroes);
+    return (
+        <TransitionGroup component="ul">
+            {elements}
+        </TransitionGroup>
+    )
+}
 
 export default HeroesList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import heroes from '../components/heroesList/heroesSlice';
 import filters from '../components/heroesFilters/filtersSlice';
+import { apiSlice } from '../api/apiSlice'; 
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -11,16 +11,11 @@ const stringMiddleware = () => (next) => (action) => {
     return next(action)
 };
 
-/* const store = createStore( 
-                combineReducers({heroes, filters}),
-                compose(applyMiddleware(thunk, stringMiddleware),
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-                ); */
-
 const store = configureStore({
-    reducer: {heroes, filters},
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
-    devTools: process.env.NODE_ENV !== 'production', 
+    reducer: {  filters, 
+                [apiSlice.reducerPath]: apiSlice.reducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
+    devTools: process.env.NODE_ENV !== 'production'
 })
 
 export default store;
